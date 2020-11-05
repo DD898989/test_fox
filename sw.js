2ca8d4d7b445c451123fa11f7e2d5bb890b7d8f5
FFFFDDDD25 <phone993825@gmail.com>
2020-11-05 16:58:09 +0800

    d
    d

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('fox-store').then(function(cache) {
+     return cache.addAll([
+       '/images/fox4.jpg'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
